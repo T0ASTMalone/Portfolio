ff2694cdac4b88240f195d2dd0254c0a421e0c81
Miguel Ponce <miguelaponce258@gmail.com>
2020-01-13 18:26:52 -0700

    fix: update nav bar for mobile

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 "use strict";
 
 function scrollTo() {
-  $(".scrollLink").on("click", function() {
-    let aid = $(this).attr("href");
-    $("html, body").animate({ scrollTop: $(aid).offset().top }, 1500);
-  });
+  const desktop = $("#scrollTo").attr("class");
+  if (desktop === "hidden") {
+    $(".scrollLink").on("click", function() {
+      $("#mobile-scrollTo").addClass("hidden");
+      let aid = $(this).attr("href");
+      $("html, body").animate({ scrollTop: $(aid).offset().top }, 1500);
+    });
+  } else {
+    $(".scrollLink").on("click", function() {
+      let aid = $(this).attr("href");
+      $("html, body").animate({ scrollTop: $(aid).offset().top }, 1500);
+    });
+  }
 }
+
+const mobileScrollTo = () => {
+  $("#menu-icon").on("click", () => {
+    const status = $("#mobile-scrollTo").attr("class");
+    if (status === "hidden") {
+      $("#mobile-scrollTo").removeClass("hidden");
+    } else {
+      $("#mobile-scrollTo").addClass("hidden");
+    }
+  });
+};
 
 function loaded() {
   scrollTo();
+  mobileScrollTo();
 }
 
 $(loaded);
